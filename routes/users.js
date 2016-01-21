271781b87cc1cac8c771a8335b73b8f6c80f9823
ebookstein <ebookstein@newrelic.com>
2016-01-21 14:01:37 -0800

    Create user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: 'learning_grid_test'
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource YAY');
 });
 
-router.get('/foo', function (req, res, next) {
-  console.log('haha')
+// Router takes a request (req), response object (res), and a callback (next)
+router.get('/:id', function (req, res, next) {
+  // connect to database
+  connection.query("SELECT * from user where id = " + req.params.id, function (err, rows) {
+    if (err || !rows.length) {
+      res.send("Cannot find this user!");
+    }
+    else {
+      res.send(rows)
+    }
+  });
 })
 module.exports = router;
